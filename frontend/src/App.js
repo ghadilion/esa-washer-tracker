0b491cd6739d96b4297e195bd137d638809d6266
hariesh-sambath <shariesh2002@gmail.com>
2023-04-29 15:54:24 +0530

    implemented sse

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
 import "./App.css";
 import Washingmachine from "./Washingmachine";
 
 function App() {
    const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+   
+   useEffect(() => {
+      const sse = new EventSource('http://localhost:5005/');
+      function getRealtimeData(data) {
+        console.log(data)
+      }
+      sse.onmessage = e => getRealtimeData(JSON.parse(e.data));
+      sse.onerror = () => {
+        // error log here 
+        sse.close();
+      }
+      return () => {
+        sse.close();
+      };
+    }, []);
 
    return (
       <div className="App">
--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5005;
 
-var bodyParser = require('body-parser')
-
-app.use(bodyParser.json())
+app.use(
+   cors({
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: false,
+   })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-   res.send("hello world");
-})
+   res.setHeader("Cache-Control", "no-cache");
+   res.setHeader("Content-Type", "text/event-stream");
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader("Connection", "keep-alive");
+   res.flushHeaders(); // flush the headers to establish SSE with client
 
-app.post('/', (req, res) => {
+   let counter = 0;
+   let interValID = setInterval(() => {
+      counter++;
+      if (counter >= 10) {
+         clearInterval(interValID);
+         res.end(); // terminates SSE session
+         return;
+      }
+      res.write(`data: ${JSON.stringify({ num: counter })}\n\n`); // res.write() instead of res.send()
+   }, 1000);
+
+   // If client closes connection, stop sending events
+   res.on("close", () => {
+      console.log("client dropped me");
+      clearInterval(interValID);
+      res.end();
+   });
+});
+
+app.post("/", (req, res) => {
    console.log(req.body);
    res.send(req.body);
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+   console.log(`Example app listening on port ${port}`);
+});
